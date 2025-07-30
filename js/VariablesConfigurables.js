4f17f0a5a0f50b5923c6c08d125dbe3b16c68be3
ProfesorSergioHidalgo <sergio.hidalgo@uempresarial.com>
2025-07-30 15:10:42 -0600

    Update VariablesConfigurables.js

--- js/VariablesConfigurables.js
+++ js/VariablesConfigurables.js
@@ -1,5 +1,5 @@
 const EXAM_NAME = "Examen de Fundamentos de TI - Código de curso TCS1003";
-const ACCESS_CODE = ""; // Código que se valida en script.js
+const ACCESS_CODE = "Shidalgo12345"; // Código que se valida en script.js
 const EXAM_DURATION_MINUTES = 120; // Cambiar a 180 u otro valor si se desea
 
 document.getElementById("title").textContent = EXAM_NAME;
